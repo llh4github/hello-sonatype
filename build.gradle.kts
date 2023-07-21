ad9e3129ccba63effdd0fe6fa1c314621051b5e4
llh <lilinhong_coding@foxmail.com>
2023-07-21 15:36:27 +0800

    build: 发布配置

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,12 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.9.0"
     id("java-library")
+    id("maven-publish")
+    id("signing")
 }
 
 group = "io.github.llh4github"
-version = "1.0-SNAPSHOT"
+version = "0.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -22,4 +24,68 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "11"
+}
+val jarSources by tasks.registering(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.map { it.allSource })
+}
+
+val jarJavadoc by tasks.registering(Jar::class) {
+    archiveClassifier.set("javadoc")
+}
+publishing {
+    publications {
+        create<MavenPublication>("dist") {
+            from(components["java"])
+            artifact(jarSources)
+            artifact(jarJavadoc)
+
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+
+            pom {
+                name.set("${project.group}:${project.name}")
+                licenses {
+                    license {
+                        name.set("The Apache Software License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+
+                scm {
+                    url.set("https://github.com/llh4github/hello-sonatype")
+                    connection.set("scm:git:https://github.com/llh4github/hello-sonatype.git")
+                    developerConnection.set("scm:git:ssh://git@github.com:llh4github/hello-sonatype.git")
+                }
+            }
+
+            repositories {
+                maven {
+                    name = "OSSRH"
+                    url = if (project.version.toString().endsWith("-SNAPSHOT")) {
+                        uri("https://s01.oss.sonatype.org/content/repositories/snapshots")
+                    } else {
+                        uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+                    }
+                    credentials {
+                        username = System.getenv("OSSRH_USER")
+                        password = System.getenv("OSSRH_PASSWORD")
+                    }
+                }
+            }
+        }
+    }
+}
+signing {
+    val keyId = System.getenv("GPG_KEY_ID")
+    val secretKey = System.getenv("GPG_SECRET_KEY")
+    val password = System.getenv("GPG_PASSWORD")
+
+    setRequired {
+        !project.version.toString().endsWith("SNAPSHOT")
+    }
+
+    useInMemoryPgpKeys(keyId, secretKey, password)
+    sign(publishing.publications["dist"])
 }
